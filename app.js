17b677f64b31ed554486d32c94c721f7619c2eec
AGUSTIN ADOLFO MELLA ZAPATA <agustin.mella@inacapmail.cl>
2022-01-12 00:54:51 -0300

    Tipos de datos

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 document.write("<h1>Hello World</h1>")
+
+//Tipos de datos
+console.log("Hello world"); // String
+console.log('Hello world'); // String
+
+// Number
+console.log(10000);
+console.log(2.5);
+console.log(3.56);
+
+//Boolean
+console.log(true);
+console.log(false);
+
+// Arrays de distintos tipos
+console.log(['joe', 'ryan', 'martha']);
+console.log([1, 2, 3]);
+console.log([true, false, true]);
+
+// Object
+console.log({
+    username: 'ryan',
+    score: 70.4,
+    hours: 14,
+    professionaL: true
+});
+
+console.log({
+    username: 'joe',
+    score: 0.4,
+    hours: 1,
+    professionaL: false
+});
+
+console.log("This is a string");
+console.log(10.4);
+console.log(true);
+console.log([1, 2, 3]);
